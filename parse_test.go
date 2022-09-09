e26e8c7b3918fb567dc378d8f75927597bd8cbd4
Derrick J. Wippler <thrawn01@gmail.com>
2022-09-09 08:11:52 -0500

    Added 'path' as a greedy version of 'string' in Path() expression (#21)

--- parse_test.go
+++ parse_test.go
@@ -2,16 +2,12 @@ package route
 
 import (
 	"net/http"
+	"testing"
 
-	. "gopkg.in/check.v1"
+	"github.com/stretchr/testify/assert"
 )
 
-type ParseSuite struct {
-}
-
-var _ = Suite(&ParseSuite{})
-
-func (s *ParseSuite) TestParseAndMatchSuccess(c *C) {
+func TestParseAndMatchSuccess(t *testing.T) {
 	testCases := []struct {
 		Expression string
 		Url        string
@@ -58,6 +54,20 @@ func (s *ParseSuite) TestParseAndMatchSuccess(c *C) {
 		{
 			`Method("POST") && Path("/helloworld/<name>")`,
 			`http://google.com/helloworld/%2F`,
+			"POST",
+			"localhost",
+			nil,
+		},
+		{
+			`Method("POST") && Path("/helloworld/<path:name>")`,
+			`http://google.com/helloworld/some/name`,
+			"POST",
+			"localhost",
+			nil,
+		},
+		{
+			`Method("POST") && Path("/escaped/<path:name>")`,
+			`http://google.com/escaped/some%2Fpath`,
 			"POST",
 			"localhost",
 			nil,
@@ -149,44 +159,44 @@ func (s *ParseSuite) TestParseAndMatchSuccess(c *C) {
 		},
 	}
 	for _, tc := range testCases {
-		comment := Commentf("%v", tc.Expression)
-		result := &match{val: "ok"}
-		p, err := parse(tc.Expression, result)
-		c.Assert(err, IsNil)
-		c.Assert(p, NotNil)
+		t.Run(tc.Expression, func(t *testing.T) {
+			result := &match{val: "ok"}
+			p, err := parse(tc.Expression, result)
+			assert.NoError(t, err)
+			assert.NotNil(t, p)
 
-		req := makeReq(req{url: tc.Url})
-		req.Method = tc.Method
-		req.Host = tc.Host
+			req := makeReq(req{url: tc.Url})
+			req.Method = tc.Method
+			req.Host = tc.Host
+			req.Header = tc.Headers
 
-		req.Header = tc.Headers
-
-		out := p.match(req)
-		c.Assert(out, NotNil, comment)
-		c.Assert(out, Equals, result, comment)
+			out := p.match(req)
+			assert.NotNil(t, p)
+			assert.Equal(t, result, out)
+		})
 	}
 }
 
-func (s *ParseSuite) TestParseFailures(c *C) {
+func TestParseFailures(t *testing.T) {
 	testCases := []string{
 		`bad`,                             // unsupported identifier
 		`bad expression`,                  // not a valid go expression
 		`Path("/path") || Path("/path2")`, // unsupported operator
-		`1 && 2`,                // unsupported statements
-		`"standalone literal"`,  // standalone literal
-		`UnknownFunction("hi")`, // unknown functin
-		`Path(1)`,               // bad argument type
-		`RegexpRoute(1)`,        // bad argument type
-		`Path()`,                // no arguments
-		`PathRegexp()`,          // no arguments
-		`Path(Path("hello"))`,   // nested calls
-		`Path("")`,              // bad trie expression
-		`PathRegexp("[[[[")`,    // bad regular expression
+		`1 && 2`,                          // unsupported statements
+		`"standalone literal"`,            // standalone literal
+		`UnknownFunction("hi")`,           // unknown function
+		`Path(1)`,                         // bad argument type
+		`RegexpRoute(1)`,                  // bad argument type
+		`Path()`,                          // no arguments
+		`PathRegexp()`,                    // no arguments
+		`Path(Path("hello"))`,             // nested calls
+		`Path("")`,                        // bad trie expression
+		`PathRegexp("[[[[")`,              // bad regular expression
 	}
 
 	for _, expr := range testCases {
 		m, err := parse(expr, &match{val: "ok"})
-		c.Assert(err, NotNil)
-		c.Assert(m, IsNil)
+		assert.Error(t, err)
+		assert.Nil(t, m)
 	}
 }
